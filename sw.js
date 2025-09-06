12cf51d2d72b8793e652434deb6ca6301def8563
jimmiandersson89-max <jimmiandersson89@gmail.com>
2025-09-06 23:19:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // ----- Priskalkylator service worker -----
 // Bumpa VERSION när du gjort större ändringar på HTML/CSS/JS/ikoner
-const VERSION = 'V1Benjii';
+const VERSION = 'V.Benjii';
 const CACHE_NAME = `rg-kalkylator-${VERSION}`;
 
 const CORE_ASSETS = [
